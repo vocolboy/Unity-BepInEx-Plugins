a51d5d708ce1b9aae066550b3af769e86e45c6d8
vocolboy <vocolboy@gmail.com>
2021-03-10 03:54:53 +0800

    feat: release craftopia BigInventory plugin

---
+++ README.md
@@ -0,0 +1,9 @@
+# Unity-BepInEx-Plugins
+
+* Craftopia 始世樂土 ( 20210225.1510 )
+    * BigInventory 背包拓展 ( 基礎 +40 格 )
+
+Inspired by [xiaoye97](https://github.com/xiaoye97) and [Mod development tutorial](https://www.bilibili.com/read/cv8997376)
+
+* Requirements
+  * https://github.com/BepInEx/BepInEx/releases/tag/v5.4.8
---
+++ craftopia/BigInventory.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Oc.Item;
+using Oc.Item.UI;
+using Oc.Missions;
+
+namespace BigInventory
+{
+    [BepInPlugin("tw.vocolboy.plugin.BigInventory", "BigInventory", "1.0.0")]
+    public class BigInventory : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            new Harmony("tw.vocolboy.plugin.BigInventory").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(OcMissionManager), "GameStartIncreaseInventory")]
+    class PatchInventory
+    {
+        public static void Postfix()
+        {
+            ItemType[] bePatch = new ItemType[4] {ItemType.Equipment, ItemType.Consumption, ItemType.Building, ItemType.Material};
+            
+            foreach (var key in bePatch)
+            {
+                SingletonMonoBehaviour<OcItemUI_InventoryMng>.Inst.IncreaseListSize(key, 40);
+            }
+        }
+    }
+}
