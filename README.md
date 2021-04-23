63fa1ed3dac57589ffbeac4873e635f1b54bb8aa
vocolboy <vocolboy@gmail.com>
2021-04-24 00:14:14 +0800

    feat: update Craftopia 20210423.1648

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Unity-BepInEx-Plugins
 
-* Craftopia 始世樂土 ( 20210225.1510 )
-  * [BigInventory](https://github.com/vocolboy/Unity-BepInEx-Plugins/releases/download/1.0.0/Craftopia_BigInventory_1.0.0.zip) 背包拓展 ( 基礎 +40 格 )
-  * [MoreSkillPoints](https://github.com/vocolboy/Unity-BepInEx-Plugins/releases/download/1.0.0/Craftopia_MoreSkillPoints_1.0.0.zip) 更多技能點 ( 基礎 * 2 )
-  * [MoreBossLoot](https://github.com/vocolboy/Unity-BepInEx-Plugins/releases/download/1.0.0/Craftopia_MoreBossLoot_1.0.0.zip) Boss掉落物增加 ( 服務器每多一人 Boss 多噴一倍 ) ( 開伺服器的裝就好 )
+* Craftopia 始世樂土 ( 20210423.1648 )
+  * BigInventory 背包拓展 ( 基礎 +40 格 )
+  * MoreSkillPoints 更多技能點 ( 基礎 * 2 )
+  * MoreBossLoot Boss掉落物增加 ( 服務器每多一人 Boss 多噴一倍 ) ( 開伺服器的裝就好 )
 
 Inspired by [xiaoye97](https://github.com/xiaoye97) and [Mod development tutorial](https://www.bilibili.com/read/cv8997376)
 
--- craftopia/MoreBossLoot.cs
+++ craftopia/MoreBossLoot.cs
@@ -3,10 +3,11 @@ using BepInEx.Logging;
 using HarmonyLib;
 using Oc;
 using Oc.Em;
+using Oc.Network;
 
 namespace MoreBossLoot
 {
-    [BepInPlugin("tw.vocolboy.plugin.MoreBossLoot", "MoreBossLoot", "1.0.0")]
+    [BepInPlugin("tw.vocolboy.plugin.MoreBossLoot", "MoreBossLoot", "1.1.0")]
     public class MoreBossLoot : BaseUnityPlugin
     {
         private Harmony harmony;
@@ -32,16 +33,18 @@ namespace MoreBossLoot
         {
             OcItemDropper dropper = Traverse.Create(__instance).Field("_ItemDropper").GetValue<OcItemDropper>();
 
-            if (!(bool)dropper)
+            if (!(bool) dropper)
             {
                 return;
             }
 
-            bool isBoss = Traverse.Create(__instance).Property("SoEm").Field("IsBoss").GetValue<bool>();
+            bool isBoss = Traverse.Create(__instance).Property("SoEm").Property("IsBoss").GetValue<bool>();
+            bool isMidBoss = Traverse.Create(__instance).Property("SoEm").Property("IsMidBoss").GetValue<bool>();
 
-            Logger.LogInfo("Is Boss " +isBoss);
-            
-            if (isBoss)
+
+            Logger.LogInfo(string.Format("Is Boss {0}, Is Mid Boss {1}", isBoss, isMidBoss));
+
+            if (isBoss || isMidBoss)
             {
                 Logger.LogInfo(string.Format("Kill Boss {0}",
                     Traverse.Create(__instance).Field("_CurLv_SoEmData")
@@ -51,7 +54,9 @@ namespace MoreBossLoot
 
                 OcNetMng inst = SingletonMonoBehaviour<OcNetMng>.Inst;
                 int[] accountIds = Traverse.Create(inst).Field("_ClientAccountId").GetValue<int[]>();
-                int onlinePlayerCount = 0;
+                int onlinePlayerCount = 1;
+
+                //old version self will set true, after 4.21 version self set false
                 for (int index = 0; index < 8; ++index)
                 {
                     if (accountIds[index] != 0)
