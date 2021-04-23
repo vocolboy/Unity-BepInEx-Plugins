78b574b1c4e3f343af37e1b2b53943e342ed2e4f
vocolboy <vocolboy@gmail.com>
2021-04-24 02:29:34 +0800

    feat: MoreSkillPoints can custom increase point

--- craftopia/MoreSkillPoints.cs
+++ craftopia/MoreSkillPoints.cs
@@ -1,15 +1,23 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using Oc;
 
 namespace MoreSkillPoints
 {
-    [BepInPlugin("tw.vocolboy.plugin.MoreSkillPoints", "MoreSkillPoints", "1.0.0")]
+    [BepInPlugin("tw.vocolboy.plugin.MoreSkillPoints", "MoreSkillPoints", "1.1.0")]
     public class MoreSkillPoints : BaseUnityPlugin
     {
+        private ConfigEntry<int> _increasePoint;
+
+        void Awake()
+        {
+            _increasePoint = Config.Bind("config", "IncreasePoint", 2, "Increase Skill Point By Level Up");
+        }
+
         private void Start()
         {
-            Traverse.Create(typeof(OcDefine)).Field("INCREASE_SKILLPOINT_BY_LEVEL_UP").SetValue(2);
+            Traverse.Create(typeof(OcDefine)).Field("INCREASE_SKILLPOINT_BY_LEVEL_UP").SetValue(_increasePoint.Value);
         }
 
         private void OnDestroy()
